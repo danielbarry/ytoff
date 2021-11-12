29c37e23d4ea4409ec79ea09bf59f39dce421656
Dan <danbarry16@googlemail.com>
2021-11-13 01:44:00 +1300

    Initial basic implementation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import sys
 import threading
 import json
@@ -10,6 +11,8 @@ from urllib.parse import urlparse
 from urllib.parse import parse_qs
 
 config = json.loads("{}")
+queue = []
+dequeue = []
 
 # MyHttpRequestHandler
 #
@@ -42,41 +45,56 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler) :
       with open(f"../raw/{video}.mp4", "rb") as file :
         self.wfile.write(file.read())
     else :
-      html = ""
       if os.path.exists(f"../raw/{video}.mp4") :
+        html = ""
         self.send_header("Content-type", config["response"]["video"]["content"])
         for s in config["response"]["video"]["html"] :
           html += s
       else :
-        yt_download(video)
-        self.send_header("Content-type", config["response"]["process"]["content"])
-        for s in config["response"]["process"]["html"] :
-          html += s
+        # Only append videos if the queue not overloaded
+        if len(queue) < config["youtube-dl"]["max-queue"] :
+          # Don't double add videos
+          if not video in queue :
+            queue.append(video)
+          html = ""
+          self.send_header("Content-type", config["response"]["process"]["content"])
+          for s in config["response"]["process"]["html"] :
+            html += s
     self.end_headers()
     # Writing the HTML contents with UTF-8
     html = html.format(
       title = config["decoration"]["title"],
       bgcolor = config["decoration"]["bgcolor"],
       fgcolor = config["decoration"]["fgcolor"],
-      code = video
+      code = video,
+      lenq = len(queue)
     )
     self.wfile.write(bytes(html, "utf8"))
     return
 
-# yt_download()
-#
-# Download a video with the given parameters.
-#
-# @param video The video code to be downloaded.
-def yt_download(video) :
-  # TODO: Check that the video code is sane and not dangerous.
-  with youtube_dl.YoutubeDL(config["youtube-dl"]["options"]) as ydl:
-    ydl.download([f"https://youtube.com/watch?v={video}"])
-  return
-
 # service_loop()
+#
+# Download videos in the queue to the target directory and remove old videos to
+# save on space.
 def service_loop() :
-  # TODO: Write this function.
+  while True :
+    # Check if we want to remove something old
+    if len(dequeue) > config["youtube-dl"]["max-dequeue"] :
+      video = dequeue[0]
+      dequeue.remove(video)
+      if os.path.exists(f"../raw/{video}.mp4") :
+        os.remove(f"../raw/{video}.mp4")
+    # Check if we want to download something new
+    if len(queue) > 0 :
+      video = queue[0]
+      # Check if ID is somewhat valid
+      if len(video) > 2 and len(video) < 16 :
+        # Blocking download
+        with youtube_dl.YoutubeDL(config["youtube-dl"]["options"]) as ydl:
+          ydl.download([f"https://youtube.com/watch?v={video}"])
+        dequeue.append(video)
+      queue.remove(video)
+    time.sleep(1)
   return
 
 # main()
